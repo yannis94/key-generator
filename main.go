6efab6f6a16cc8c6773e6e44ff561eb7f6bc390c
yannis94 <bengueci.yannis@gmail.com>
2022-03-25 12:04:35 +0100

    test setup

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "github.com/yannis94/key-generator"
+)
+
+func main() {
+    fmt.Println("Key generator")
+    // 1. call introduction func 
+    // 2. get user choice
+    // 3. redirect to corresponding func
+    // 4. output result
+}
